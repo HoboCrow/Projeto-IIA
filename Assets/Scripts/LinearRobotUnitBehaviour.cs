c83d8941239b860e97be5c0ae2464830693a6d44
Olex <30861670+Olex@users.noreply.github.com>
2020-02-29 20:29:35 +0000

    initial commit

---
+++ Assets/Scripts/LinearRobotUnitBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class LinearRobotUnitBehaviour : RobotUnit
+{
+    public float weightResource;
+    public float resourceValue;
+    public float resouceAngle;
+
+    public float weightObstacle;
+    public float obstacleValue;
+    public float obstacleAngle;
+
+    void Update()
+    {
+
+        // get sensor data
+        resouceAngle = resourcesDetector.GetAngleToClosestResource();
+
+        resourceValue = weightResource * resourcesDetector.GetLinearOuput();
+
+        // apply to the ball
+        applyForce(resouceAngle, resourceValue); // go towards
+
+        obstacleAngle = blockDetector.GetAngleToClosestObstacle();
+
+        obstacleValue = weightObstacle * blockDetector.GetLinearOuput();
+
+        applyForce(obstacleAngle, obstacleValue);
+        
+
+    }
+
+
+}
+
+
+
+
+
+
