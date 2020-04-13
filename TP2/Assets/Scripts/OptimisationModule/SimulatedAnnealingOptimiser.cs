1088fde2412040afed2532a91281c548da3ade6a
Oleksandr Yakovlyev <olex.yakov97@gmail.com>
2020-04-13 19:56:33 +0100

    Algumas funções de simulated annealing

--- TP2/Assets/Scripts/OptimisationModule/SimulatedAnnealingOptimiser.cs
+++ TP2/Assets/Scripts/OptimisationModule/SimulatedAnnealingOptimiser.cs
@@ -1,26 +1,36 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEditor;
 using UnityEngine;
 
 public class SimulatedAnnealingOptimiser : OptimisationAlgorithm
 {
     private List<int> newSolution = null;
     private int CurrentSolutionCost;
-    private enum function { LOG10 };
-    [SerializeField]
-    private function SelectedFunction;
+    public enum function { LOG10, GEOMETRIC, LINEAR, ARITH_GEO };
+    public function SelectedFunction;
+    [HideInInspector]
+    public float InitialTemperature;
     public float Temperature;
     private float zero = Mathf.Pow(10, -6);// numbers bellow this value can be considered zero.
 
     string fileName = "Assets/Logs/" + System.DateTime.Now.ToString("ddhmmsstt") + "_SimulatedAnnealingOptimiser.csv";
 
+    [SerializeField]
+    [HideInInspector]
+    private float logConst;
+    [SerializeField]
+    [HideInInspector]
+    private float geoConst;
 
     protected override void Begin()
     {
         CreateFileSA(fileName);
         // Initialization
         // Temp inicializada fora
+        InitialTemperature = Temperature;
         CurrentSolution = GenerateRandomSolution(targets.Count);
         CurrentSolutionCost = Evaluate(CurrentSolution);
 
@@ -31,7 +41,6 @@ public class SimulatedAnnealingOptimiser : OptimisationAlgorithm
 
     protected override void Step()
     {
-
         newSolution = GenerateNeighbourSolution(CurrentSolution);
         int newSolutionCost = Evaluate(newSolution);
 
@@ -43,24 +52,65 @@ public class SimulatedAnnealingOptimiser : OptimisationAlgorithm
             CurrentSolutionCost = newSolutionCost;
         }
 
-        Temperature = TemperatureSchedule(Temperature);
+        Temperature = TemperatureSchedule(currTemp: Temperature, iter: CurrentNumberOfIterations);
 
         //DO NOT CHANGE THE LINES BELLOW
         AddInfoToFile(fileName, base.CurrentNumberOfIterations, CurrentSolutionCost, CurrentSolution, Temperature);
         base.CurrentNumberOfIterations++;
     }
 
-    private float TemperatureSchedule(float temp)
+    private float TemperatureSchedule([Optional] float currTemp, [Optional] int iter)
     {
         float newTemp;
         switch (SelectedFunction)
         {
             case function.LOG10:
-                newTemp = temp / 1.1f;
+                newTemp = 1 / (Mathf.Log10(1 + iter));
                 break;
+            case function.GEOMETRIC:
+                newTemp = InitialTemperature * Mathf.Pow(geoConst, iter);
+                break;
+            case function.LINEAR:
+                throw new NotImplementedException();
+            case function.ARITH_GEO:
+                throw new NotImplementedException();
             default:
-                throw new Exception($"Invalid Temperature function : enum value {SelectedFunction}. Check if value is set.");
+                throw new Exception($"Invalid Temperature function call : enum value {SelectedFunction}. Check if value is set.");
         }
         return newTemp > zero ? newTemp : zero;
+    }
+}
+
+[CustomEditor(typeof(SimulatedAnnealingOptimiser))]
+[CanEditMultipleObjects]
+public class SimulatedAnnealingOptimiserEditor : Editor
+{
+    SerializedProperty logConstProperty;
+    SerializedProperty geoConstProperty;
+    private void OnEnable()
+    {
+        logConstProperty = serializedObject.FindProperty("logConst");
+        geoConstProperty = serializedObject.FindProperty("geoConst");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+        var sam = target as SimulatedAnnealingOptimiser;
+        switch (sam.SelectedFunction)
+        {
+            case SimulatedAnnealingOptimiser.function.LOG10:
+                EditorGUILayout.PropertyField(logConstProperty, new GUIContent("C"));
+                break;
+            case SimulatedAnnealingOptimiser.function.GEOMETRIC:
+                EditorGUILayout.PropertyField(geoConstProperty, new GUIContent("alfa"));
+                break;
+            case SimulatedAnnealingOptimiser.function.LINEAR:
+                break;
+            case SimulatedAnnealingOptimiser.function.ARITH_GEO:
+                break;
+        }
+        serializedObject.ApplyModifiedProperties();
     }
 }
